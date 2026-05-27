1c5626caeda7d02dacb390fa6bca6968cacbc6e7
vreyes-hash <vreyes@wavesinmovement.com>
2026-05-27 11:41:21 -0300

    Add files via upload

---
+++ config (3).js
@@ -0,0 +1,45 @@
+const CONFIG = {
+
+  linkedin: {
+    clientId:       'PEGAR_CLIENT_ID_AQUI',
+    clientSecret:   'PEGAR_CLIENT_SECRET_AQUI',
+    organizationId: 'PEGAR_ORG_ID_AQUI',
+    accessToken:    'PEGAR_ACCESS_TOKEN_AQUI',
+  },
+
+  instagram: {
+    accessToken: 'PEGAR_ACCESS_TOKEN_AQUI',
+    accountId:   'PEGAR_ACCOUNT_ID_AQUI',
+  },
+
+  youtube: {
+    apiKey:      'PEGAR_API_KEY_AQUI',
+    channelId:   'PEGAR_CHANNEL_ID_AQUI',
+    accessToken: 'PEGAR_ACCESS_TOKEN_AQUI',
+  },
+
+  odoo: {
+    url:      'https://TU_INSTANCIA.odoo.com',
+    database: 'NOMBRE_BASE_DE_DATOS',
+    apiKey:   'PEGAR_API_KEY_AQUI',
+  },
+
+  ga4: {
+    propertyId:    '365569053',
+    measurementId: 'G-2BH9V872M1',
+    serviceAccountKey: {
+      "type": "service_account",
+      "project_id": "psychic-bliss-400823",
+      "private_key_id": "f0ebfac8f93487acad1520ec2fffa6fe40b5e05d",
+      "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC0N1cSd0L/6TCD\nyI0ysDsAEUQV4FFDcsM2xUyVxeQcfXAdW7u2QBXPBo82RFfv1JsBsHGH3RIoST0C\nX37x/8Y4T3nKyTKTXEJShvjyk8FSnMPnntguoYwigN01hJhXF+yW/Huf5BWAPFhZ\n8OsX0DbRunJtZhkTagFmvfR1jgp8qi49+30q0DRq5Oqbnir91YsaohVa0O26780D\nUmo9sFSdX5u5s7TXwCIp67tWRF8lLifo5SXhGqThPO+a2lWm3TJQ3LGqISQjGYAW\nG4Wbhjie0wwUC5qyOuoNoWh/S3bPlGPNgvEvJcTCG6cIpczUi3nUeC1qD1Cnvtes\n6JGSWpOFAgMBAAECggEADjG3psmEROzEeJ9mA6KJcHFrspS8QdA7BkgsRor+Ha0B\nlMZiDAeTJX8HmPIcF6ngBZY0naCwv+yOJMQ0bEdwK4ToUQEKKJIhL9KCrsnVdbYe\n4KeMW8E3QjZydeHPhTiUrAsPNPeS0UqveMdhdOWI6SRCw/wWfdwhEnW5WJL19NM+\nsrk+C1scBe6/0+RR4NUN/y5pQ88mNfbcudxYQrvXm4UxM8NksFTrbGK8uOXZrC8w\nCjLq/OomtlrbfLYpK+t2qsUxpz2dW9Bcz113n9t75fAm0JJevRddb9Evh3hgVNrG\niHpY4Tcq3kxycq8fDqyQzLKEJYqDHIr6qRk+P9TTtwKBgQDkWrKuHOqsCjZFNc03\n9dhYUrvzG/2tR0f1J0PgDuylzM3sCTuk7HoRABlR2sniwolRBNJMpZa81022LC+o\nrYHIDMi/0z6gNRl+xWXF6GJGf+lujh7GHzT5jjQaMX1/LaVZo4r//sjkkvRo/8oH\nY7WAy5Lk/IEPsswUWhtUkbpg2wKBgQDKCLcC+8D6pnTyOKcgpp8NF/lrqNi5BPK9\nCMeOK9EQluI98icNLh9BNr3dvgoRSrzl9w/wIPeFppWbLAMiPsQ81XJ1qETbXE3r\nsF1SZK8x/HVPsIjOndikOGBaTFJHjSXyWqj2FEPXvPyrZJKEHBr131gm9PdjGaCd\nnGvy+oFbHwKBgB68SnJ+9+MiGL3mCPIwR+dTc3K8YL+S6Y+Ism0U9Ss3zrunXnzL\nS37ASagnz6aS0pKyGHAylYBjZlVPyzZj7CT1+4s+Lp63pu/WN+WUaq3ch5nWzTOT\nfbjdOAwSfza/+RKAnbPpIjcJxJXhX9rww7xLKkFh7ykiwq82lZkjaDejAoGAduU6\nDp9pqhhbqlB8ylJEN8hsGFd9cyTeVTDdf2V523FwI1RmdvM1oYM9qUk1U4fQMuV6\nk65S/GxUjsUP8w9NHR/gBC3Kl+1yTcm0odBu6dPQld0mJbHu1HX4/ir8IO+H7WA9\nHtQ+OLAXYiYFs4l2tEuD7SksjT1/sEFlpdCWX7ECgYBDHNNW8Z5the3OJcKPKWYc\n0mvMV2upSZbqz5+b4izyAMhqi6gTSFZFVBVrK10Aqrki08A5R5m2gE2AfbZR6+BF\ndX6Qq6ZgBK8WQG4w6iTmSrEL6aOPuQ+/dqCbT4YSNTp6t81gAJW0hXCZVOZGOH+I\nVDOyyH08ld23Ga5VwVdvxg==\n-----END PRIVATE KEY-----\n",
+      "client_email": "waves-dashboard@psychic-bliss-400823.iam.gserviceaccount.com",
+      "client_id": "115356507589660037954",
+      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+      "token_uri": "https://oauth2.googleapis.com/token",
+      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/waves-dashboard%40psychic-bliss-400823.iam.gserviceaccount.com",
+      "universe_domain": "googleapis.com"
+    },
+  },
+
+};
